d46bc7af13adb9823e03491623eaba301997e331
민권 송 <llimsg150@gmail.com>
2023-03-06 20:05:10 +0900

    tab 자동완성 업데이트

--- src/cli.cpp
+++ src/cli.cpp
@@ -10,8 +10,54 @@
 #define CLI_ARG_BUFFER_SIZE 100
 
 #ifdef _WIN32
+	#include <conio.h>
 	#define STRCPY strcpy_s
-	#define STRTOK strtok_s
+	#define STRTOK strtok_s	
+
+
+	void input_cmd(char* input_cmdBuf) {
+	int cmdBuf_idx = 0;
+	char tempChar;
+	do {
+		tempChar = _getch();
+		if (cmdBuf_idx <= 0) {
+			cmdBuf_idx = 0;
+			input_cmdBuf[0] = '\0';
+		}
+		if (tempChar == '\t') {
+			bool stopIterFlag = false;
+			for (CLI_BLOCK* blockIter = CLI_BLOCK_HEAD; blockIter != NULL; blockIter = blockIter->next) {
+
+				for (int tabCmpIdx = 0; tabCmpIdx < strlen(blockIter->command); tabCmpIdx++) {
+					if (input_cmdBuf[tabCmpIdx] != blockIter->command[tabCmpIdx]) {
+						if (tabCmpIdx < cmdBuf_idx)	break;
+						else {
+							stopIterFlag = true;
+							_putch(blockIter->command[tabCmpIdx]);
+							input_cmdBuf[cmdBuf_idx++] = blockIter->command[tabCmpIdx];
+						}
+					}
+				}
+
+				if (stopIterFlag) break;
+			}
+
+		}
+		else if (tempChar == '\b' && cmdBuf_idx > 0) {
+			input_cmdBuf[--cmdBuf_idx] = '\0';
+			_putch('\b');
+			_putch(' ');
+			_putch('\b');
+		}
+		else {
+			input_cmdBuf[cmdBuf_idx++] = tempChar;
+			_putch(tempChar);
+		}
+	} while (tempChar != '\r');
+
+	input_cmdBuf[cmdBuf_idx - 1] = '\0';
+
+}
 #else
 	#define STRCPY strcpy
 	#define STRTOK strtok_r
@@ -60,12 +106,16 @@ void CLI_GET_ARG(char* dest, int arg_index) {
 	printf("%s %s\n", VV_stringBuf[0],dest);
 }
 
+
 bool TERMINAL_CONTROL_CLI() {
 	char commandBuffer[CLI_ARG_BUFFER_SIZE];
 	std::cout << "esp cli >> ";
+#ifdef _WIN32
+	input_cmd(commandBuffer);
+#else
 	fgets(commandBuffer, CLI_ARG_BUFFER_SIZE, stdin);
 	commandBuffer[strlen(commandBuffer) - 1] = NULL;
-	
+#endif
 	for (CLI_BLOCK* blockIter = CLI_BLOCK_HEAD; blockIter != NULL; blockIter = blockIter->next) {
 		if (command_cmp(blockIter->command, commandBuffer)) {
 			blockIter->cli_function();
